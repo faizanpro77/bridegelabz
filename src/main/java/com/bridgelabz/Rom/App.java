daeaca0b7f059a61b4a3f83778eccbcca83a9ea2
faizanpro77 <faizyshaikh204@gmail.com>
2021-05-19 17:05:42 +0530

    ADDED:fuctionality arithmatic

--- src/main/java/com/bridgelabz/Rom/App.java
+++ src/main/java/com/bridgelabz/Rom/App.java
@@ -9,10 +9,13 @@ public class App
 	
     public static void main( String[] args )
     {
+    	int a=10,b=20;
+    	int c=a+b;
         System.out.println( "Hello World!" );
         System.out.println( "World!" );
         System.out.println( "arbaz" );
         System.out.println( "fool");
         System.out.println("faizan");
+	System.out.println("new one");
     }
 }
